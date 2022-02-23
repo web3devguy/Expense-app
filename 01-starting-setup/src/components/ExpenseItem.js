73379026c9c92481facdae226d1fd6b7d4e48518
insideoutzombie <daviddbruno9@gmail.com>
2022-02-23 15:52:54 -0500

    adds date feature

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -4,7 +4,12 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div>
+        <div></div>
+        <div></div>
+        {/* {props.date.toISOString()} */}
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
