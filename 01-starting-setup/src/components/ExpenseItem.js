1a7d44864a314e64ed696d554da0aeb2e69bb483
insideoutzombie <daviddbruno9@gmail.com>
2022-02-23 17:34:26 -0500

    adds component for date

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-
 function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>
-        <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div>
-        <div></div>
-        <div></div>
-        {/* {props.date.toISOString()} */}
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
