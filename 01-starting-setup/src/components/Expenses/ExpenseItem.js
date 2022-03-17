10b3bd29e1823b67faf88a78857bf8a6a9002b54
insideoutzombie <daviddbruno9@gmail.com>
2022-03-17 18:56:35 -0400

    changes to array and button syntax

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,21 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
 
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated';
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Test Me Here!</button>
     </Card>
   );
 }
